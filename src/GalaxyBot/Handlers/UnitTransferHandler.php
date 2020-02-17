27f87d206f55c7075bdf41da396f980c7c3eb3bc
HarpyWar <harpywar@gmail.com>
2020-02-17 14:23:03 +0300

    transfer all units from low resource planets (sure except hercules) and part of units from other resource type planets if full supply

--- src/GalaxyBot/Handlers/UnitTransferHandler.php
+++ src/GalaxyBot/Handlers/UnitTransferHandler.php
@@ -12,6 +12,8 @@ use GalaxyBot\Types\MineralType;
 
 class UnitTransferHandler extends PlanetHandler
 {
+    private $supply_limit_percents = 95; // %
+
     public function Execute()
     {
         if (!parent::Execute())
@@ -22,19 +24,49 @@ class UnitTransferHandler extends PlanetHandler
         $user = $this->account->user;
         $api = $this->account->api;
 
-        // send loki and valkyries to orbital station
+        // how many units we must send to orbital station, because of full supply (95% filled)
+        $send_quantity = 0;
+        $supply_limit = $p->supply / 100 * $this->supply_limit_percents;
+        if ($p->used_supply > $supply_limit)
+        {
+            $send_quantity = $p->used_supply - $supply_limit;
+            $api->log($this->supply_limit_percents . "% supply used, we must send " . $send_quantity . " units to orbital");
+        }
+
+        // iterate every type of units
         foreach ($p->units as $u)
         {
-            if ($u->id != UnitType::Loki && $u->id != UnitType::Valkyrie) 
+            if ($u->id == UnitType::Hercules)
                 continue;
-            if ($u->quantity == 0)
+
+            $quantity = $u->quantity;
+            if ($quantity == 0)
                 continue;
+
+            // for not green, yellow and blue planets - transfer all units except hercules
+            if ($send_quantity <= 0)
+            {
+                if ($p->resource_id > MineralType::Otarium)
+                {
+                    if ($u->id != UnitType::Loki && $u->id != UnitType::Valkyrie)
+                        continue;
+                }
+            }
+            else
+            {
+                // send only required units
+                // 10 - 100 = -90
+                // 100 + (-90) = 10
+                $send_quantity -= $quantity;
+                if ($send_quantity <= 0)
+                    $quantity += $send_quantity;
+            }
+
             // find trade center
             if ($tc_id = GalaxyHelper::FindBuilding(BuildingType::Trade, $p->grids))
             {
-                $api->log("send " . $u->id . " (" . $u->quantity . ")");
-                $api->SendUnit($u->id, $u->quantity, $tc_id);
-                break;
+                $api->log("send " . $u->id . " (" . $quantity . ")");
+                $api->SendUnit($u->id, $quantity, $tc_id);
             }
         }
     }
